540e55c9e322d03c3ff3cee4a1e47e26eacf4d81
CodeWithkaran02 <7870karuna@gmail.com>
2023-03-22 11:00:15 +0530

    and or not program

---
+++ Not.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Not {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if (n > 0 != n < 10) {
+            System.out.println("false");
+
+        } else {
+            System.out.println("true");
+        }
+    }
+}
---
+++ OR.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class OR {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if (n > 0 || n < 10) {
+            System.out.println("true");
+
+        } else {
+
+            System.out.println("false");
+        }
+
+    }
+
+}
