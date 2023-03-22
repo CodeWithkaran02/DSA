2a307add79753809465f17e6a860d84e60573c57
CodeWithkaran02 <7870karuna@gmail.com>
2023-03-22 12:03:05 +0530

    toPrintSum

---
+++ toPrintsum.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class toPrintsum {
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum = 0;
+        // for (int i = 1; i <= n; i++) {
+        // sum = sum + i;
+        // }
+        for (int i = 1; i <= n; i = i + 1) {
+            sum = (n * (n + 1)) / 2;
+        }
+        System.out.println(sum);
+    }
+
+}
