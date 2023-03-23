7c8cd2ecb86e9fa5d28bc48af88c9b71a59e97fa
CodeWithkaran02 <7870karuna@gmail.com>
2023-03-23 13:00:35 +0530

    count/reverse

---
+++ countdigtino.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class countdigtino {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 1; i <= n.length; i++) {
+            System.out.println(n);
+        }
+    }
+
+}
