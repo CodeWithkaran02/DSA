00d82a6d2ae5c5f6581beb24222551b3458f99e2
CodeWithkaran02 <7870karuna@gmail.com>
2023-03-26 00:35:32 +0530

    last row 1star

---
+++ patten3.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class patten3 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int row = 1; row <= n; row++) {
+            int nst = row;
+            int nsp = n - nst;
+
+            for (int i = 1; i <= nsp; i++) {
+                System.out.print("\t");
+            }
+            for (int i = 1; i <= nst; i++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+}
