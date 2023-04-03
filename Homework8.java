7143678a5c927df5ca6c5bf522f30aa6760399b0
CodeWithkaran02 <7870karuna@gmail.com>
2023-04-03 23:01:08 +0530

    area

---
+++ Homework8.java
@@ -0,0 +1,9 @@
+public class Homework8 {
+
+    public static void main(String args[]) {
+        float h = 4, b = 13, area;
+        area = (h * b) / 2;
+        System.out.println(area);
+
+    }
+}
