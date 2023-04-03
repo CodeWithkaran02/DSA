609d3b54ad642e2003f7e9b5b0ef61e6a09a2ed2
CodeWithkaran02 <7870karuna@gmail.com>
2023-04-03 22:58:09 +0530

    A+B+C/2

---
+++ Homework4.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Homework4 {
+    public static void main(String args[]) {
+        int S;
+
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        System.out.println("A:");
+
+        int B = sc.nextInt();
+        System.out.println("B");
+
+        int C = sc.nextInt();
+        System.out.println("C");
+        S = (A + B + C) / 2;
+
+        System.out.println(S);
+
+    }
+}
