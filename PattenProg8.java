2ea7bcfebb5811cb990bef1cd039c03bfc5af3ab
CodeWithkaran02 <7870karuna@gmail.com>
2023-04-03 22:46:47 +0530

    1to 25

--- PattenProg8.java
+++ PattenProg8.java
@@ -1,4 +1,5 @@
 public class PattenProg8 {
+
     public static void main(String args[]) {
         int n = 5;
         for (int i = 1; i <= n; i++) {
@@ -7,4 +8,5 @@ public class PattenProg8 {
             }
             System.out.println();
         }
+    }
 }
