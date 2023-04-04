e01ed2622a2cf0d8e0128cd2b90e1319dca60a61
CodeWithkaran02 <7870karuna@gmail.com>
2023-04-04 23:46:58 +0530

    add spme file

---
+++ PattenProg10.java
@@ -0,0 +1,12 @@
+public class PattenProg10 {
+
+    public static void main(String args[]) {
+        int n = 5;
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; i <= 3; j++) {
+                System.out.print(i + "" + j + "");
+            }
+            System.out.println();
+        }
+    }
+}
--- PattenProg8.java
+++ PattenProg8.java
@@ -1,5 +1,4 @@
 public class PattenProg8 {
-
     public static void main(String args[]) {
         int n = 5;
         for (int i = 1; i <= n; i++) {
@@ -8,5 +7,4 @@ public class PattenProg8 {
             }
             System.out.println();
         }
-    }
 }
