096684c59989422bd36fbc03746ef86edcc7adfa
CodeWithkaran02 <7870karuna@gmail.com>
2023-04-11 23:28:10 +0530

    Matrix sum

---
+++ MatrixSum.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class MatrixSum {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a element ");
+        int x = sc.nextInt();
+        int a[] = new int[x];
+        int oddsum = 0;
+        int evenodd = 0;
+
+        for (int i = 0; i < a.length; i++) {
+            a[i] = sc.nextInt();
+        }
+        for (int j = 0; j < a.length; j++) {
+            if (a[j] % 2 == 0) {
+                evenodd = oddsum + a[j];
+
+            } else {
+                oddsum = oddsum + a[j];
+
+            }
+            System.out.println("Sum of Even Number is :=" + evenodd);
+            System.out.println("Sum of Odd Nymber is := " + oddsum);
+        }
+
+    }
+}
