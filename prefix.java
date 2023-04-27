b96a3e3e86e6fa79662421e50e01b9d120d4933b
CodeWithkaran02 <7870karuna@gmail.com>
2023-04-27 23:24:14 +0530

    prefix

---
+++ prefix.java
@@ -0,0 +1,12 @@
+class prefix {
+    public static void main(String[] abc) {
+        int A = 20;
+        System.out.println(A++);
+        System.out.println(--A);
+        System.out.println(++A);
+        System.out.println(A--);
+        System.out.println(A);
+        System.out.println(A++);
+        System.out.println(--A);
+    }
+}
