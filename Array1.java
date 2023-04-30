f7d1c74854c9ab40750de826ec73a2821f8a4a96
CodeWithkaran02 <7870karuna@gmail.com>
2023-04-30 23:10:29 +0530

    index

---
+++ Array1.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Array1 {
+    public static void main(String agsp[]) {
+        int ave = 30;
+        int phy = 97;
+        int chen = 98;
+        int eng = 95;
+
+        int[] marks = new int[3];
+        marks[0] = 97;
+        marks[1] = 98;
+        marks[2] = 95;
+        System.out.println(marks[0]);
+    }
+}
