d22878da5dc60c9308dec92d07ddc038ba72725d
CodeWithkaran02 <7870karuna@gmail.com>
2023-05-05 23:58:57 +0530

    broken ecomany

---
+++ brokenEconamy.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class brokenEconamy {
+    public static void main(String args[]) {
+
+        {
+            Scanner sc = new Scanner(System.in);
+            int n = sc.nextInt();
+            int arr[] = new int[n];
+            int k = sc.nextInt();
+            binaryarray(arr, k);
+
+        }
+    }
+
+    public static int binaryarray(int[] arr, int k) {
+        int lo = 0, hi = arr.length - 1;
+        int ceil = -1, floue = -1;
+        while (lo <= hi) {
+            int mid = (lo + hi) / 2;
+            if (k < arr[mid]) {
+                hi = mid - 1;
+                ceil = arr[mid];
+
+            } else if (k > arr[mid]) {
+                lo = mid + 1;
+                floue = arr[mid];
+                return;
+            }
+        }
+        System.out.println(ceil);
+        System.out.println(floue);
+    }
+}
