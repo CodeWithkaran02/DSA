a118179f038d94390506cadbf3e9164154c115a3
CodeWithkaran02 <7870karuna@gmail.com>
2023-05-14 21:44:09 +0530

    anthore method for print 2D array

---
+++ TwoDdemo.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class TwoDdemo {
+    public static void input(int mat[][], Scanner sc) {
+        for (int r = 0; r < mat.length; r++) {
+            for (int c = 0; c < mat[0].length; c++) {
+                mat[r][c] = sc.nextInt();
+
+            }
+        }
+    }
+
+    public static void display(int mat[][]) {
+        for (int r = 0; r < mat.length; r++) {
+            for (int c = 0; c < mat[0].length; c++) {
+                System.out.print(mat[r][c] + " ");
+            }
+        }
+        System.out.println();
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int nr = sc.nextInt();
+        int nc = sc.nextInt();
+        int[][] mat = new int[nr][nc];
+        input(mat, sc);
+        display(mat);
+    }
+}
