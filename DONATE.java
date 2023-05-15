113cce0ebb316bdd618326addc7de468a52bc1be
CodeWithkaran02 <7870karuna@gmail.com>
2023-05-15 23:22:41 +0530

    blood donate

---
+++ DONATE.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class DONATE {
+    public static void main(String abc[]) {
+        int age = 20;
+        int weight = 80;
+        if (age > 20) {
+            if (weight > 50) {
+                System.out.println("you are eligible to donate blood");
+
+            }
+        }
+    }
+}
