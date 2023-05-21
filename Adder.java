58810a3000fffd65a98389ff5efa17d40fafe771
CodeWithkaran02 <7870karuna@gmail.com>
2023-05-21 23:58:43 +0530

    adder

---
+++ Adder.java
@@ -0,0 +1,22 @@
+class Adder
+{
+static int add(int a , int b )
+{
+	return a+b;
+	
+}
+static double add(double a, double b)
+{
+	return a+b;
+	
+}
+class textOverloadding2
+{
+	public static void main (String args[])
+	{
+		System.out.println(Adder.add(11,11));
+		System.out.println(Adder.add(12.6,12.6));
+		
+	}
+}
+}
