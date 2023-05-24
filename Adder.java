63d8c83f119e64e920acd7293b3a60f53b95eb25
CodeWithkaran02 <7870karuna@gmail.com>
2023-05-24 23:28:14 +0530

    wakanda series

--- Adder.java
+++ Adder.java
@@ -1,22 +1,19 @@
-class Adder
-{
-static int add(int a , int b )
-{
-	return a+b;
-	
-}
-static double add(double a, double b)
-{
-	return a+b;
-	
-}
-class textOverloadding2
-{
-	public static void main (String args[])
-	{
-		System.out.println(Adder.add(11,11));
-		System.out.println(Adder.add(12.6,12.6));
-		
-	}
-}
+class Adder {
+    static int add(int a, int b) {
+        return a + b;
+
+    }
+
+    static double add(double a, double b) {
+        return a + b;
+
+    }
+
+    class textOverloadding2 {
+        public static void main(String args[]) {
+            System.out.println(Adder.add(11, 11));
+            System.out.println(Adder.add(12.6, 12.6));
+
+        }
+    }
 }
