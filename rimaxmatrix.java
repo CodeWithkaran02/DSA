c5ac936626a70c9a639304a31957327562dff3db
CodeWithkaran02 <7870karuna@gmail.com>
2023-05-30 23:37:19 +0530

    remix matrix

---
+++ rimaxmatrix.java
@@ -0,0 +1,18 @@
+
+import java.util.*;
+
+public class rimaxmatrix {
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int nr = sc.nextInt();
+        int nc = sc.nextInt();
+        int arr[][] = new int[nr][nc];
+
+        for (int i = 0; i < nr; i++) {
+            for (int j = 0; j < nc; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+    }
+}
