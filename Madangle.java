f255514e46ec3dab052fe7b48108f2eaaac710a7
CodeWithkaran02 <7870karuna@gmail.com>
2023-06-12 23:03:13 +0530

    mad angle patten

--- Madangle.java
+++ Madangle.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Mad_angles {
+public class Madangle {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
