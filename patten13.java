b56391b550491716b4015b527ba1cac98816fe8d
CodeWithkaran02 <7870karuna@gmail.com>
2023-06-15 23:48:55 +0530

    patten 1+base

--- patten13.java
+++ patten13.java
@@ -3,6 +3,21 @@ import java.util.*;
 public class patten13 {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        int num = sc.nextInt();
+        for (int row = 0; row < num; row++) {
+            int ncr = 0;
+
+            for (int col = 0; col <= row; col++) {
+                System.out.print(ncr + "\t");
+                int n = row, r = col;
+
+                int ncrp1 = ((n - r) * ncr) / (r + 1);
+
+                ncr = ncrp1;
+
+            }
+        }
+        System.out.println();
     }
+
 }
