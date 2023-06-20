62a55dd9cbb9f9877e93447a4735d05a062508d9
CodeWithkaran02 <7870karuna@gmail.com>
2023-06-20 22:57:28 +0530

    string charAt

---
+++ Stringpbm.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Stringpbm {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("hello");
+
+        for (int i = 0; i < sb.length() / 2; i++) {
+
+            int front = i;
+            int back = sb.length() - 1 - i;
+
+            char frontChar = sb.charAt(front);
+            char backChar = sb.charAt(back);
+
+            sb.setCharAt(back, frontChar);
+            sb.setCharAt(front, backChar);
+        }
+        System.out.println(sb);
+    }
+
+}
--- ringrotete.java
+++ ringrotete.java
@@ -3,6 +3,126 @@ import java.util.*;
 public class ringrotete {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        int nr = sc.nextInt();
+        int nc = sc.nextInt();
+        int mat[][] = new int[nr][nc];
+
+        for (int i = 0; i < nr; i++) {
+            for (int j = 0; j < nc; j++) {
+                mat[i][j] = sc.nextInt();
+
+            }
+        }
+        int shell = sc.nextInt();
+        int k = sc.nextInt();
+
+        ringrotete(mat, shell, k);
+        display(mat);
+
+    }
+
+    public static void ringrotete(int[][] mat, int shell, int k) {
+        Integer arr[] = fill1d(mat, shell);
+        rotate(arr, k);
+        fill2d(mat, shell, arr);
+    }
+
+    public static Integer[] fill1d(int mat[][], int shell, int r) {
+        int nr = mat.length, nc = mat[0].length;
+        int rmin = shell - 1, cmin = shell - 1;
+        int rmax = nr - shell, cmax = nc - shell;
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        // left wall
+        for (int r = rmin; r <= rmax; r++) {
+            list.add(mat[r][cmin]);
+
+        }
+        // botton wall
+        for (int c = cmin - 1; c >= rmin; c--) {
+            list.add(mat[r][max]);
+        }
+
+        // right wall
+        for (int r = rmax - 1; r >= rmin; r--) {
+            list.add(mat[r][cmax]);
+        }
+        // top wall
+        for (int c = cmax - 1; c >= cmin + 1; c--) {
+            list.add(mat[rmin][c]);
+
+        }
+        int n = list.size();
+        return list.toArray(new Integer[n]);
+
+    }
+
+    public static void fill2d(int[][] mat, int shell, Integer[] arr) {
+        int nr = mat.length, nc = mat[0].length;
+        int rmin = shell - 1, cmin = shell - 1;
+        int rmax = nr - shell, cmax = nc - shell;
+        int idx = 0;
+
+        // left wall
+        for (int r = rmin; r < rmax; r++) {
+            mat[r][cmin] = arr[idx];
+            idx++;
+
+        }
+        // botton wall
+        for (int c = cmin + 1; c <= cmax; c++) {
+            mat[rmax][c] = arr[idx];
+
+        }
+        // right wall
+
+        for (int r = rmax - 1; r >= rmin; r--) {
+            mat[r][cmax] = arr[idx];
+            idx++;
+
+        }
+
+        // top wall
+
+        for (int c = cmax - 1; c >= cmin + 1; c--) {
+            mat[rmin][c] = arr[idx];
+            idx++;
+
+        }
+    }
+
+    public static void reverse(Integer[] a, int li, int ri) {
+        while (li < ri) {
+            int temp = a[li];
+            a[li] = a[ri];
+            a[ri] = temp;
+            li++;
+            ri--;
+
+        }
+
+    }
+
+    public static void rotate(Integer[] a, int k) {
+        k = k % a.length;
+        if (k < 0) {
+            k = +a.length;
+
+        }
+
+        reverse(a, 0, a.length, k - 1);
+        reverse(a, a.length - k, a.length - 1);
+        reverse(a, 0, a.length - 1);
+    }
+
+    public static void display(Integer[][] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                System.out.print(arr[i][i] + " ");
+
+            }
+            System.out.println();
+        }
     }
 }
