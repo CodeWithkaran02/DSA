eb939b4e2cdb774adbf0afab4d1418b4d754d26c
CodeWithkaran02 <7870karuna@gmail.com>
2023-06-23 21:56:27 +0530

    concadinet

---
+++ Recersion/Stringpblm.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Stringpblm {
+    public static void main(String args[]) {
+        String s1 = new String("hello");
+        String s2 = new String("World");
+        String s = String.format("%s%s", s1, s2);
+
+        System.out.println(s.toString());
+    }
+}
